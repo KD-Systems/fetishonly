e91be49abfde7813f66a2c5a20c824c68fa7549e
Sujan Molla <smitexpert@gmail.com>
2023-11-27 12:43:10 +0600

    ADD | Deployment script

---
+++ deployment/deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "Stopping application..."
+
+cd ./../
+
+
+php artisan down
+
+echo "Start Installing..."
+
+
+echo "#1 Pulling git"
+git pull
+
+echo "#2 Installing composer"
+composer install --no-interaction --prefer-dist --optimize-autoloader
+
+
+echo "#3 Running migration"
+php artisan migrate --force
+
+echo "#4 clearing caches"
+php artisan optimize:clear
+
+
+echo "#5 Optimizing caches"
+php artisan config:cache
+php artisan event:cache
+php artisan route:cache
+php artisan view:cache
+
+echo "#6 Starting application..."
+php artisan up
