e9363f8e1728f32b94acbc9e8c4b6d07e71a874e
Sujan Molla <smitexpert@gmail.com>
2023-12-07 23:18:30 +0600

    UPDATE | Twiter access

--- resources/views/elements/settings/settings-twitter.blade.php
+++ resources/views/elements/settings/settings-twitter.blade.php
@@ -5,7 +5,7 @@
     You are account is connected with twitter.
 </button>
 @else
-<a href="https://twitter.com/i/oauth2/authorize?response_type=code&client_id=MGRmRnYyenktVTItWXhCd01ZOEw6MTpjaQ&redirect_uri={{ env('X_REDIRECT_URI', 'https://example.com') }}&scope=tweet.write tweet.read users.read offline.access&state=state&code_challenge=challenge&code_challenge_method=plain" class="btn btn-info rounded mr-0 d-flex align-items-center justify-content-center" type="submit">
+<a href="https://twitter.com/i/oauth2/authorize?response_type=code&client_id={{ env('X_CLIENT_ID') }}&redirect_uri={{ env('X_REDIRECT_URI', 'https://example.com') }}&scope=tweet.write tweet.read users.read offline.access&state=state&code_challenge=challenge&code_challenge_method=plain" class="btn btn-info rounded mr-0 d-flex align-items-center justify-content-center" type="submit">
     @include('elements.icon',['icon'=> 'logo-twitter','centered'=>'false','classes'=>'mr-3','variant'=>'medium'])
     {{__('ConnectWithTwitter')}}</a>
 @endif
