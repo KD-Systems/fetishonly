1bd0fc0591fbae7d2705a08a3a4d3af3405348f0
Sujan Molla <smitexpert@gmail.com>
2024-02-18 21:03:53 +0600

    UPDATE | CODE|

--- resources/views/template/head.blade.php
+++ resources/views/template/head.blade.php
@@ -60,11 +60,11 @@
 @endif
 
 @if (config('app.enable_hotjar', false))
-    <!-- Hotjar Tracking Code for my site -->
+    <!-- Hotjar Tracking Code for Fetishonly -->
     <script>
         (function(h,o,t,j,a,r){
             h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:3870332,hjsv:6};
+            h._hjSettings={hjid:3870764,hjsv:6};
             a=o.getElementsByTagName('head')[0];
             r=o.createElement('script');r.async=1;
             r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
