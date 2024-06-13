0fcbfcc7e8da155b55cf34afbc435907a6c0c632
Sujan Molla <smitexpert@gmail.com>
2024-06-14 02:03:08 +0600

    UPDATE | ADD CHECK

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -73,7 +73,7 @@
                 </div>
                 @if (!Auth::check() || Auth::user()->id !== $user->id)
                     <div class="">
-                        <small class="p-0 m-0">{{trans_choice('fans', $user->fansCount, ['number'=> count(ListsHelper::getUserFollowers(Auth::user()->id))])}} - {{trans_choice('following', Auth::user()->followingCount, ['number'=>$user->followingCount])}}</small>
+                        <small class="p-0 m-0">{{trans_choice('fans', $user->fansCount, ['number'=> count(ListsHelper::getUserFollowers(Auth::user()->id))])}} - {{trans_choice('following', Auth::user()->followingCount, ['number'=>$user->followingCount])}} Of the users</small>
                     </div>
                 @else
                     <div class="">
