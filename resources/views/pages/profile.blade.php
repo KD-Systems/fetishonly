b6b8c181bf71836265a0f0d8074d7e65a1d50470
Sujan Molla <smitexpert@gmail.com>
2024-06-14 02:01:39 +0600

    UPDATE | ADD CHECK

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -71,9 +71,15 @@
                 <div class="z-index-3 avatar-holder">
                     <img src="{{$user->avatar}}" class="rounded-circle">
                 </div>
-                <div class="">
-                    <small class="p-0 m-0">{{trans_choice('fans', Auth::user()->fansCount, ['number'=> count(ListsHelper::getUserFollowers(Auth::user()->id))])}} - {{trans_choice('following', Auth::user()->followingCount, ['number'=>Auth::user()->followingCount])}}</small>
-                </div>
+                @if (!Auth::check() || Auth::user()->id !== $user->id)
+                    <div class="">
+                        <small class="p-0 m-0">{{trans_choice('fans', $user->fansCount, ['number'=> count(ListsHelper::getUserFollowers(Auth::user()->id))])}} - {{trans_choice('following', Auth::user()->followingCount, ['number'=>$user->followingCount])}}</small>
+                    </div>
+                @else
+                    <div class="">
+                        <small class="p-0 m-0">{{trans_choice('fans', Auth::user()->fansCount, ['number'=> count(ListsHelper::getUserFollowers(Auth::user()->id))])}} - {{trans_choice('following', Auth::user()->followingCount, ['number'=>Auth::user()->followingCount])}}</small>
+                    </div>
+                @endif
                 <div>
                     @if(!Auth::check() || Auth::user()->id !== $user->id)
                         <div class="d-flex flex-row">
